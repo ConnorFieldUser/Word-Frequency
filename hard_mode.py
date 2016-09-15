5cfd8d9464fb07703b5245fd07064d679221df2f
ConnorFieldUser <conquest.field@gmail.com>
2016-09-15 13:00:11 -0400

    More changes, biggest problems cleared.

--- hard_mode.py
+++ hard_mode.py
@@ -14,13 +14,22 @@ holmes_scrubbed = holmes_scrubbed.split(' ')
 
 words_in_holmes = {}
 
-bad_word = ['if', 'so', '', 'the', 'it', 'these', 'the', 'that', 'and', 'to', 'of', 'in', 'it', 'is', 'his']
+bad_word = """a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,
+because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,
+for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,
+it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,
+not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,
+since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,
+twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,
+would,yet,you,your"""
+
+bad_word = bad_word.replace('\n', ',')
+bad_word = bad_word.split(',')
 
 
 for wordin in holmes_scrubbed:
-    if wordin in holmes_scrubbed == bad_word:
-        for bad in bad_word:
-            holmes_scrubbed.replace(bad, '')
+    if wordin in bad_word:
+        pass
     elif wordin in words_in_holmes.keys():
         words_in_holmes[wordin] += 1
     else:
@@ -36,4 +45,4 @@ count = 0
 
 for word, number in final_list_20:
     new_num = (number/50)
-    print(word + ': ' + str(number) + '#' * new_num)
+    print(word + ': ' + '#' * new_num)
