9da5b2e941a7a20b95690b9fc2e3af9288e4b0bd
ConnorFieldUser <conquest.field@gmail.com>
2016-11-11 15:39:37 -0500

    aadded  model

--- wordfrequency_django/app/models.py
+++ wordfrequency_django/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class SherlockBook(models.Model):
+    text = models.TextField()
+
+    def __str__(self):
+        return "Sherlock Holmes"
--- wordfrequency_django/app/views.py
+++ wordfrequency_django/app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from django.views.generic import TemplateView, DetailView
+from app.models import SherlockBook
 # Create your views here.
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
+class SherlockBookDetailView(DetailView):
+    model = SherlockBook
